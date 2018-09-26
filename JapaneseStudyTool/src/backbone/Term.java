c46956888690a0381f63c19c19392a2608db8e39
Jacob Wechsler <frogkabobs@gmail.com>
2018-09-25 19:03:25 -0500

    Term

--- JapaneseStudyTool/src/backbone/Term.java
+++ JapaneseStudyTool/src/backbone/Term.java
@@ -1,5 +1,11 @@
 package backbone;
 
 public class Term {
-
+	public String term;
+	public String description;
+	public String[] tags;
+	
+	public Term(String s, String d, String... tags) {
+		
+	}
 }
