ee2f4b36c05b76f6d6854b9bc6f28a988d33f929
Jacob Wechsler <frogkabobs@gmail.com>
2018-09-25 19:34:24 -0500

    asd

--- JapaneseStudyTool/src/backbone/Term.java
+++ JapaneseStudyTool/src/backbone/Term.java
@@ -5,7 +5,11 @@ public class Term {
 	public String description;
 	public String[] tags;
 	
-	public Term(String s, String d, String... tags) {
-		
+	public Term(String s, String d, String... t) {
+		term = s;
+		description = d;
+		tags = t; 
 	}
+	
+	
 }
---
+++ JapaneseStudyTool/src/backbone/TermsList.java
@@ -0,0 +1,20 @@
+package backbone;
+
+import java.io.Serializable;
+import java.util.Comparator;
+
+import javafx.collections.FXCollections;
+import javafx.collections.transformation.SortedList;
+
+public class TermsList implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public SortedList<Term> terms;
+	
+	public TermsList(Term... t) {
+		terms = new SortedList<>(FXCollections.observableArrayList(t), (u,v) -> Comparator.comparing(String::toString).compare(u.term, v.term));
+	}
+
+}
